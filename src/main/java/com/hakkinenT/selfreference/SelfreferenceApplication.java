788b3a2e2c9a1a2a71bdbfc19464c7c4146b91a0
Tawanna <thol_16@hotmail.com>
2024-05-23 20:55:35 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/selfreference/SelfreferenceApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.selfreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfreferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/selfreference/SelfreferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.selfreference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfreferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
