ee51558620bb938f637cde0aa0cd43aa7e40217c
Tawanna <thol_16@hotmail.com>
2024-05-24 20:33:30 -0300

    Changed name of DataIntegrityViolationException to DatabaseException

--- src/main/java/com/hakkinenT/selfreference/controllers/exceptions/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/hakkinenT/selfreference/controllers/exceptions/handlers/ControllerExceptionHandler.java
@@ -1,2 +1,29 @@
-package com.hakkinenT.selfreference.controllers.exceptions.handlers;public class ControllerExceptionHandler {
+package com.hakkinenT.selfreference.controllers.exceptions.handlers;
+
+import com.hakkinenT.selfreference.controllers.exceptions.dto.StandardError;
+import com.hakkinenT.selfreference.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Resource Not Found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
 }
--- src/main/java/com/hakkinenT/selfreference/services/exceptions/DataIntegrityViolationException.java
+++
@@ -1,7 +0,0 @@
-package com.hakkinenT.selfreference.services.exceptions;
-
-public class DataIntegrityViolationException extends RuntimeException{
-    public DataIntegrityViolationException(String message) {
-        super(message);
-    }
-}
