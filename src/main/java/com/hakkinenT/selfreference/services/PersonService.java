83cb44b0c0a7e01bb24de3ccf3238cb5ce0ec30e
Tawanna <thol_16@hotmail.com>
2024-05-24 22:04:12 -0300

    findById, findAll and update methods implemented

--- src/main/java/com/hakkinenT/selfreference/services/PersonService.java
+++ src/main/java/com/hakkinenT/selfreference/services/PersonService.java
@@ -5,11 +5,14 @@ import com.hakkinenT.selfreference.entities.Person;
 import com.hakkinenT.selfreference.repositories.PersonRepository;
 import com.hakkinenT.selfreference.services.exceptions.DatabaseException;
 import com.hakkinenT.selfreference.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PersonService {
@@ -44,4 +47,33 @@ public class PersonService {
         }
     }
 
+    @Transactional(readOnly = true)
+    public PersonDTO findById(Long id){
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso " + id + "não encontrado!"));
+        return new PersonDTO(person);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PersonDTO> findAll(){
+        List<Person> person = personRepository.findAll();
+        return person.stream().map(PersonDTO::new).toList();
+    }
+
+    @Transactional
+    public PersonDTO update(Long id, PersonDTO dto){
+        try{
+            Person person = personRepository.getReferenceById(id);
+            person.setName(dto.getName());
+
+            Person partner = new Person();
+            partner.setName(dto.getPartner().getName());
+            person.setPartner(partner);
+
+            person = personRepository.save(person);
+            return new PersonDTO(person);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Resource " + id + " não encontrado!");
+        }
+    }
 }
--- src/test/java/com/hakkinenT/selfreference/services/PersonServiceTests.java
+++ src/test/java/com/hakkinenT/selfreference/services/PersonServiceTests.java
@@ -6,6 +6,7 @@ import com.hakkinenT.selfreference.repositories.PersonRepository;
 import com.hakkinenT.selfreference.services.exceptions.DatabaseException;
 import com.hakkinenT.selfreference.services.exceptions.ResourceNotFoundException;
 import com.hakkinenT.selfreference.tests.PersonFactory;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class PersonServiceTests {
@@ -58,6 +62,14 @@ public class PersonServiceTests {
 
         Mockito.doNothing().when(personRepository).deleteById(existingPersonId);
         Mockito.doThrow(DataIntegrityViolationException.class).when(personRepository).deleteById(dependentId);
+
+        Mockito.when(personRepository.findById(existingPersonId)).thenReturn(Optional.of(person));
+        Mockito.when(personRepository.findById(nonExistingPersonId)).thenReturn(Optional.empty());
+
+        Mockito.when(personRepository.findAll()).thenReturn(List.of(person));
+
+        Mockito.when(personRepository.getReferenceById(existingPersonId)).thenReturn(person);
+        Mockito.when(personRepository.getReferenceById(nonExistingPersonId)).thenThrow(EntityNotFoundException.class);
     }
 
     @Test
@@ -87,6 +99,43 @@ public class PersonServiceTests {
     public void deleteShouldThrowDatabaseExceptionWhenDependentId(){
         Assertions.assertThrows(DatabaseException.class, () -> {
             personService.delete(dependentId);
+        });
+    }
+
+    @Test
+    public void findByIdShouldReturnPersonDTOWhenIdExists(){
+        PersonDTO result = personService.findById(existingPersonId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getId(), existingPersonId);
+    }
+
+    @Test
+    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists(){
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            personService.findById(nonExistingPersonId);
+        });
+    }
+
+    @Test
+    public void findAllShouldReturnPersonDTOList(){
+        List<PersonDTO> result = personService.findAll();
+        Assertions.assertEquals(result.get(0).getId(), personDTO.getId());
+        Assertions.assertEquals(result.get(0).getName(), personDTO.getName());
+    }
+
+    @Test
+    public void updateShouldReturnPersonDTOWhenIdExists(){
+        PersonDTO result = personService.update(existingPersonId, personDTO);
+
+        Assertions.assertEquals(result.getId(), personDTO.getId());
+        Assertions.assertEquals(result.getName(), personDTO.getName());
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists(){
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            personService.update(nonExistingPersonId, personDTO);
         });
     }
 }
